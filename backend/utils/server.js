cc9c2158375897281c05a5fa68d4ce82ca86d02f
NumaNaval <numanaval3@gmail.com>
2024-05-23 11:08:12 +0530

    new changes

--- backend/utils/server.js
+++ backend/utils/server.js
@@ -2,12 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js";
-import userRoutes from "./routes/user.routes.js";
-import connectToMongoDB from "./db/connectToMongoDB.js";
+import authRoutes from "../routes/auth.routes.js";
+import messageRoutes from "../routes/message.routes.js";
+import userRoutes from "../routes/user.routes.js";
+import connectToMongoDB from "../db/connectToMongoDB.js";
+import {app, server} from '../socket/socket.js'
 
-const app= express();
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -23,7 +24,7 @@ app.use("/api/auth",authRoutes);
     // res.send("Hello World!!");
  //})
  
-app.listen(PORT,()=> {
+server.listen(PORT,()=> {
     connectToMongoDB();
 console.log(`Server running on port ${PORT}`)
 });
